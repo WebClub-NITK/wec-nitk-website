829b0f676a2f694da989464933d54ccae74c5921
Shubham-Rasal <bluequbits@gmail.com>
2024-02-02 16:08:54 +0530

    add framer and shadcn

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button"
+import TestAnimation from "@/components/framer/test";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <Button>Click me</Button>
+        <TestAnimation />
+    </main>
+  );
+}
