4609c367dfc37e62cf70d5e024741c2ceab33b05
mshreeharsha <shreeharsha.mbhat@gmail.com>
2024-02-23 21:48:27 +0530

    Blogs: Add Search and Filter Bar

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,16 +1,28 @@
+"use client";
 import Card from '@/components/blogs/Card'
+import SearchAndFilter from '@/components/blogs/SearchAndFilter';
 import {blogs} from '@/lib/blogStaticData'
+import React, { useState } from 'react';
+
 const page = () => {
+
+  const [selectedCategory,setSelectedCategory] = useState('')
+  const [typedInput,setTypedInput]=useState('')
+
   return (
-    <div className="px-32 py-8 max-xl:px-8 max-xl:py-4 max-md:px-16 max-sm:px-8 m-auto">
+    <div className="px-32 py-8 max-xl:px-8 max-xl:py-4 max-md:px-16 max-sm:px-8 m-auto flex flex-col justify-center items-center">
         <h1 className="text-center text-4xl font-bold">Blog</h1>
-        {/* Search bar and Tag dropdown*/}
-        <h3 className="mb-3 mt-6 text-center text-2xl">Search bar and Tag dropdown</h3>
+        <SearchAndFilter 
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory} 
+          typedInput={typedInput} 
+          setTypedInput={setTypedInput}
+        />
         <div className="grid grid-cols-2 gap-4 max-md:grid-cols-1">
-            {blogs.map((blog,index)=>(
-                <>
-                    <Card key={index} blog={blog}/>
-                </>
+            {blogs.map((blog) => (
+              <React.Fragment key={blog.id}>
+                <Card blog={blog} />
+              </React.Fragment>
             ))}
         </div>
     </div>
