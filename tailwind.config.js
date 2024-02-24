1b1ba34a1a9c6b5cfdc3846518725600febc8e62
Vishwas1687 <97346738+Vishwas1687@users.noreply.github.com>
2024-02-24 11:15:10 +0530

    fix-card-width-responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -98,6 +98,9 @@ module.exports = {
         "3xl": "40rem",
         "4xl": "60rem",
       },
+      screens: {
+        xs: "500px",
+      },
       keyframes: {
         scroll: {
           to: {
