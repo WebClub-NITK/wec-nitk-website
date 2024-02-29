6cc4324e2e541b061b31608a9b5d99b3585828bd
Shubham-Rasal <bluequbits@gmail.com>
2024-03-01 02:23:46 +0530

    add spider

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,15 @@
+import { LampDemo } from "@/components/lamp-effect";
 import Image from "next/image";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-end justify-between bg-secondary-blue">
-      <nav className="h-16 w-full z-10 bg-white"></nav>
-
-      <div className="absolute h-full w-full flex items-center justify-end">
-        <Image
-          className="opacity-50 relative z-10 object-cover"
-          src="/logo.png"
-          alt="illustration"
-          width={500}
-          height={500}
-        />
-      </div>
-      <div className="h-screen flex justify-end items-center w-full relative">
-          <div className="h-16 w-1/2 bg-white">
-
-          </div>
-      </div>
+    <main className="flex w-screen min-h-screen flex-col items-center justify-center bg-slate-950 p-2">
+      <Image
+        className="z-0 object-contain"
+        src="/logo.svg"
+        alt="illustration"
+        width={500}
+        height={500}
+      />
     </main>
   );
 }
