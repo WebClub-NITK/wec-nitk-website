435236b707aaf2a3e4599584d026e5e3e974cffd
Shubham-Rasal <bluequbits@gmail.com>
2024-03-10 15:14:05 +0530

    add pulse

--- src/app/alumni/page.js
+++ src/app/alumni/page.js
@@ -3,8 +3,320 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex w-screen min-h-screen flex-col items-start justify-center  bg-slate-950 p-2">
-      
+    <main className="flex w-screen min-h-screen flex-col items-start justify-center bg-secondary-800 p-2">
+      <div data-illustration="true">
+        <div className="animated-streaming_root__gIlRt">
+          <svg
+            className="animated-streaming_grid__LB6Qj"
+            height="258"
+            viewBox="0 0 392 258"
+            width="392"
+          >
+            <g
+              opacity="0.08"
+              stroke="var(--geist-foreground)"
+              strokeDasharray="1 1"
+            >
+              <line x2="392" y1="15.5" y2="15.5"></line>
+              <line x2="392" y1="31.5" y2="31.5"></line>
+              <line x2="392" y1="47.5" y2="47.5"></line>
+              <line x2="392" y1="63.5" y2="63.5"></line>
+              <line x2="392" y1="79.5" y2="79.5"></line>
+              <line x2="392" y1="95.5" y2="95.5"></line>
+              <line x2="392" y1="111.5" y2="111.5"></line>
+              <line x2="392" y1="127.5" y2="127.5"></line>
+              <line x2="392" y1="143.5" y2="143.5"></line>
+              <line x2="392" y1="159.5" y2="159.5"></line>
+              <line x2="392" y1="175.5" y2="175.5"></line>
+              <line x2="392" y1="191.5" y2="191.5"></line>
+              <line x2="392" y1="207.5" y2="207.5"></line>
+              <line x2="392" y1="223.5" y2="223.5"></line>
+              <line x2="392" y1="239.5" y2="239.5"></line>
+              <line x2="392" y1="255.5" y2="255.5"></line>
+              <line x1="11.9999" x2="11.9999" y1="2.18557e-08" y2="256"></line>
+              <line x1="27.9999" x2="27.9999" y1="2.18557e-08" y2="256"></line>
+              <line x1="43.9999" x2="43.9999" y1="2.18557e-08" y2="256"></line>
+              <line x1="59.9999" x2="59.9999" y1="2.18557e-08" y2="256"></line>
+              <line x1="75.9999" x2="75.9999" y1="2.18557e-08" y2="256"></line>
+              <line x1="91.9999" x2="91.9999" y1="2.18557e-08" y2="256"></line>
+              <line x1="108" x2="108" y1="2.18557e-08" y2="256"></line>
+              <line x1="124" x2="124" y1="2.18557e-08" y2="256"></line>
+              <line x1="140" x2="140" y1="2.18557e-08" y2="256"></line>
+              <line x1="156" x2="156" y1="2.18557e-08" y2="256"></line>
+              <line x1="172" x2="172" y1="2.18557e-08" y2="256"></line>
+              <line x1="188" x2="188" y1="2.18557e-08" y2="256"></line>
+              <line x1="204" x2="204" y1="2.18557e-08" y2="256"></line>
+              <line x1="220" x2="220" y1="2.18557e-08" y2="256"></line>
+              <line x1="236" x2="236" y1="2.18557e-08" y2="256"></line>
+              <line x1="252" x2="252" y1="2.18557e-08" y2="256"></line>
+              <line x1="268" x2="268" y1="2.18557e-08" y2="256"></line>
+              <line x1="284" x2="284" y1="2.18557e-08" y2="256"></line>
+              <line x1="300" x2="300" y1="2.18557e-08" y2="256"></line>
+              <line x1="316" x2="316" y1="2.18557e-08" y2="256"></line>
+              <line x1="332" x2="332" y1="2.18557e-08" y2="256"></line>
+              <line x1="348" x2="348" y1="2.18557e-08" y2="256"></line>
+              <line x1="364" x2="364" y1="2.18557e-08" y2="256"></line>
+              <line x1="380" x2="380" y1="2.18557e-08" y2="256"></line>
+            </g>
+          </svg>
+          <div className="animated-streaming_window__8eDmq">
+            <div data-toolbar="true">
+              <div data-control="true"></div>
+              <div data-control="true"></div>
+              <div data-control="true"></div>
+            </div>
+            <div data-content="true">
+              <div
+                className="stack_stack__iZkUS stack"
+                data-version="v1"
+                style={{
+                  "--stack-flex": "initial",
+                  "--stack-direction": "row",
+                  "--stack-align": "stretch",
+                  "--stack-justify": "flex-start",
+                  "--stack-padding": "0px",
+                  "--stack-gap": "16px",
+                }}
+              >
+                <div
+                  className="stack_stack__iZkUS stack"
+                  data-version="v1"
+                  style={{
+                    "--stack-flex": "initial",
+                    "--stack-direction": "column",
+                    "--stack-align": "stretch",
+                    "--stack-justify": "flex-start",
+                    "--stack-padding": "0px",
+                    "--stack-gap": "8px",
+                  }}
+                  data-column-first="true"
+                >
+                  <div
+                    data-avatar="true"
+                    style={{
+                      borderRadius: "2px",
+                      width: "32px",
+                      height: "32px",
+                    }}
+                  >
+                    <svg
+                      aria-label="Vercel logomark"
+                      height="10"
+                      role="img"
+                      style={{ width: "auto", overflow: "visible" }}
+                      viewBox="0 0 74 64"
+                    >
+                      <path
+                        d="M37.5896 0.25L74.5396 64.25H0.639648L37.5896 0.25Z"
+                        fill="var(--accents-3)"
+                      ></path>
+                    </svg>
+                  </div>
+                  <div
+                    className="stack_stack__iZkUS stack"
+                    data-version="v1"
+                    style={{
+                      "--stack-flex": "initial",
+                      "--stack-direction": "column",
+                      "--stack-align": "stretch",
+                      "--stack-justify": "flex-start",
+                      "--stack-padding": "0px",
+                      "--stack-gap": "4px",
+                    }}
+                  >
+                    <div data-line="true" data-size="full"></div>
+                    <div
+                      data-line="true"
+                      data-size="half"
+                      data-variant="dim"
+                    ></div>
+                    <div data-line="true" data-variant="dim"></div>
+                    <div data-line="true" data-variant="dim"></div>
+                    <div
+                      data-line="true"
+                      data-size="half"
+                      data-variant="dim"
+                    ></div>
+                  </div>
+                </div>
+                <div data-column-second="true">
+                  <div
+                    className="stack_stack__iZkUS stack"
+                    data-version="v1"
+                    style={{
+                      "--stack-flex": "initial",
+                      "--stack-direction": "row",
+                      "--stack-align": "center",
+                      "--stack-justify": "space-between",
+                      "--stack-padding": "0px",
+                      "--stack-gap": "0px",
+                    }}
+                    data-header="true"
+                  >
+                    <div data-avatar="true"></div>
+                    <div data-btn="true"></div>
+                  </div>
+                  <div
+                    className="stack_stack__iZkUS stack"
+                    data-version="v1"
+                    style={{
+                      "--stack-flex": "initial",
+                      "--stack-direction": "row",
+                      "--stack-align": "center",
+                      "--stack-justify": "flex-start",
+                      "--stack-padding": "0px",
+                      "--stack-gap": "4px",
+                    }}
+                    data-nav="true"
+                  >
+                    <div data-line="true" style={{ width: "16px" }}></div>
+                    <div data-line="true" data-variant="dim"></div>
+                  </div>
+                  <div data-rectangles="true"></div>
+                  <div>
+                    <div data-rectangle="true" style={{ height: "48px" }}></div>
+                    <div data-rectangle="true" style={{ height: "48px" }}></div>
+                    <div data-rectangle="true" style={{ height: "48px" }}></div>
+                  </div>
+                  <div>
+                    <div
+                      data-rectangle="true"
+                      data-size="large"
+                      style={{ height: "32px" }}
+                    ></div>
+                    <div
+                      data-rectangle="true"
+                      data-size="large"
+                      style={{ height: "32px" }}
+                    ></div>
+                    <div
+                      data-rectangle="true"
+                      data-size="large"
+                      style={{ height: "32px" }}
+                    ></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <svg
+          className="animated-streaming_pulse__Ri1UL"
+          data-position="bottom-left"
+          fill="none"
+          height="35"
+          viewBox="0 0 237 35"
+          width="237"
+        >
+          <path
+            stroke="url(#:R2h6dpu6rta:)"
+            strokeLinecap="round"
+            strokeWidth="1.6894171745225322"
+            d="M0.5 33.4999L59 33.5C59.5523 33.5 60 33.0619 60 32.5096C60 28.5146 60 13.282 60 2.49741C60 1.94512 60.4477 1.49997 61 1.49997L91 1.49997C91.5523 1.49997 92 1.94769 92 2.49997L92 10C92 14.1422 95.3579 17.5 99.5 17.5L236 17.5001"
+          ></path>
+          <defs>
+            <linearGradient
+              gradientUnits="userSpaceOnUse"
+              id=":R2h6dpu6rta:"
+              x1="244.73225855617784"
+              x2="116.75107562390622"
+              y1="148.7698926916346"
+              y2="92.80752682709135"
+            >
+              <stop stopColor="#3291FF" stopOpacity="0"></stop>
+              <stop stopColor="#3291FF"></stop>
+              <stop offset="1" stopColor="#61DAFB" stopOpacity="0"></stop>
+            </linearGradient>
+          </defs>
+        </svg>
+        <svg
+          className="animated-streaming_pulse__Ri1UL"
+          data-position="bottom-right"
+          fill="none"
+          height="67"
+          viewBox="0 0 221 67"
+          width="221"
+        >
+          <path
+            stroke="url(#:R3h6dpu6rta:)"
+            strokeLinecap="round"
+            strokeWidth="1.5457195237686392"
+            d="M220.5 1.5H178C177.448 1.5 177 1.94772 177 2.5V32.5C177 33.0523 176.552 33.5 176 33.5H130C129.448 33.5 129 33.9477 129 34.5V58C129 62.1421 125.642 65.5 121.5 65.5H1"
+          ></path>
+          <defs>
+            <linearGradient
+              gradientUnits="userSpaceOnUse"
+              id=":R3h6dpu6rta:"
+              x1="83.33304927597055"
+              x2="115.09774011999252"
+              y1="176.86311270168517"
+              y2="83.8746308546979"
+            >
+              <stop stopColor="#3291FF" stopOpacity="0"></stop>
+              <stop stopColor="#3291FF"></stop>
+              <stop offset="1" stopColor="#61DAFB" stopOpacity="0"></stop>
+            </linearGradient>
+          </defs>
+        </svg>
+        <svg
+          className="animated-streaming_pulse__Ri1UL"
+          data-position="top-left"
+          fill="none"
+          height="51"
+          viewBox="0 0 237 51"
+          width="237"
+        >
+          <path
+            stroke="url(#:R4h6dpu6rta:)"
+            strokeLinecap="round"
+            strokeWidth="1"
+            d="M0.5 1.5L43 1.5C43.5523 1.5 44 1.94772 44 2.5L44 48.5C44 49.0523 44.4477 49.5 45 49.5L91 49.5C91.5523 49.5 92 49.0523 92 48.5L92 41C92 36.8579 95.3579 33.5 99.5 33.5L236 33.5"
+          ></path>
+          <defs>
+            <linearGradient
+              gradientUnits="userSpaceOnUse"
+              id=":R4h6dpu6rta:"
+              x1="380"
+              x2="260"
+              y1="0"
+              y2="50"
+            >
+              <stop stopColor="#3291FF" stopOpacity="0"></stop>
+              <stop stopColor="#3291FF"></stop>
+              <stop offset="1" stopColor="#61DAFB" stopOpacity="0"></stop>
+            </linearGradient>
+          </defs>
+        </svg>
+        <svg
+          className="animated-streaming_pulse__Ri1UL"
+          data-position="top-right"
+          fill="none"
+          height="209"
+          viewBox="0 0 130 209"
+          width="130"
+        >
+          <path
+            stroke="url(#:R5h6dpu6rta:)"
+            strokeLinecap="round"
+            strokeWidth="1"
+            d="M129 0.5V95C129 95.5523 128.552 96 128 96H66C65.4477 96 65 96.4477 65 97V200C65 204.142 61.6421 207.5 57.5 207.5H1"
+          ></path>
+          <defs>
+            <linearGradient
+              gradientUnits="userSpaceOnUse"
+              id=":R5h6dpu6rta:"
+              x1="-220"
+              x2="-160"
+              y1="160"
+              y2="80"
+            >
+              <stop stopColor="#3291FF" stopOpacity="0"></stop>
+              <stop stopColor="#3291FF"></stop>
+              <stop offset="1" stopColor="#61DAFB" stopOpacity="0"></stop>
+            </linearGradient>
+          </defs>
+        </svg>
+      </div>
     </main>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-import { BackgroundBeams } from "@/components/background-beams";
+import { BackgroundBeams, Logo } from "@/components/background-beams";
 import { AccordionDemo } from "@/components/faq";
 import Footer from "@/components/footer";
 import { Accordion } from "@radix-ui/react-accordion";
@@ -9,27 +9,30 @@ export default function Home() {
     <main className="w-full h-full">
       <section className="flex h-screen flex-col items-start justify-center  bg-secondary-800">
         <BackgroundBeams />
-        <div className="relative w-full flex items-center justify-center">
+        <div className="relative w-full flex flex-col h-full justify-center items-center">
           <Image
-            className="absolute z-0 opacity-85 object-contain  "
+            className="absolute z-0  object-contain bg-transparent animate-pulse"
             src="/logo.svg"
             alt="illustration"
-            width={400}
-            height={400}
+            width={1000}
+            height={1000}
           />
+          {/* <Logo 
+          className="absolute z-0 opacity-95 object-contain grow"
+          /> */}
           <div
-            className=" absolute h-24 w-full border-y 
-          bg-secondary-800 z-10 opacity-80
-          border-cyan-900 text-white lg:text-6xl font-bold text-center flex  justify-center items-center"
+            className=" absolute h-24 w-full 
+          bg-secondary-800 z-10 opacity-85 text-xl
+           text-white lg:text-4xl font-bold text-center flex  justify-center items-center 
+           bottom-0
+           "
           >
             We are Web Enthusiasts` Club NITK
           </div>
         </div>
       </section>
       <section className="flex flex-col items-center px-4 justify-between w-full h-screen bg-secondary-800 text-white">
-        <span className="lg:text-6xl text-3xl font-bold py-12">
-          FAQ
-        </span>
+        <span className="lg:text-6xl text-3xl font-bold py-12">FAQ</span>
         <div className="flex flex-col lg:flex-row w-full h-full ">
           <p className="lg:w-1/2 px-12">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
