aacf24eeca5ad33a619214e6c86e89828dfb4161
Hariharan <jhariharan2004@gmail.com>
2024-05-19 20:47:23 +0530

    Modified filters in members page to use dropdown

--- src/app/members/Filters.jsx
+++ src/app/members/Filters.jsx
@@ -1,27 +1,39 @@
 "use client"
 
+import { Button } from "@/components/ui/button"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuRadioGroup, DropdownMenuRadioItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { FilterIcon } from "lucide-react"
+
 export default function Filters ({ selected, select }) {
-    return (
-        <div className="flex gap-8 justify-center mt-10">
-            <Tab select={select} selected={selected} i={0}>All</Tab>
-            <Tab select={select} selected={selected} i={1}>Algo</Tab>
-            <Tab select={select} selected={selected} i={2}>Intel</Tab>
-            <Tab select={select} selected={selected} i={3}>GDSC</Tab>
-            <Tab select={select} selected={selected} i={4}>Systems</Tab>
-            <Tab select={select} selected={selected} i={5}>Alumni</Tab>
-        </div>
-    )
-}
+    const filters = ["Algorithms", "Intelligence", "GDSC", "Systems & Security"]
 
-function Tab ({ children, select, selected, i }) {
-    let active = selected == i
+    let selectedText = "All"
+    if (selected == 5) selectedText = "Alumni"
+    else if (selected > 0) selectedText = filters[selected-1]
 
     return (
-        <div
-            className={`py-1 px-8 border border-primary-500 ${active ? "animate-shimmer bg-[linear-gradient(110deg,#016198,45%,#0177BD,55%,#016198_65%)] bg-[length:200%_100%]" : "hover:bg-primary-500/30 duration-200"} transition-colors rounded-md cursor-pointer`}
-            onClick = {() => select(i)}
-        >
-            {children}
+        <div className="flex justify-end -mt-16">
+            <DropdownMenu className="p-2">
+                <DropdownMenuTrigger asChild>
+                    <Button className="flex items-center gap-2 m-2 hover:bg-primary-50 text-secondary-700" variant="outline">
+                        <FilterIcon className="w-4 h-4" />
+                        {selectedText}
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end" className="w-48">
+                    <DropdownMenuRadioGroup value={selected}>
+                        <DropdownMenuRadioItem value={0} onClick={() => select(0)} className="cursor-pointer">All</DropdownMenuRadioItem>
+                        {
+                            filters.map((filter, i) => {
+                                return (
+                                    <DropdownMenuRadioItem key={filter} value={i+1} onClick={() => select(i+1)} className="cursor-pointer">{filter}</DropdownMenuRadioItem>
+                                )
+                            })
+                        }
+                        <DropdownMenuRadioItem value={5} onClick={() => select(5)} className="cursor-pointer">Alumni</DropdownMenuRadioItem>
+                    </DropdownMenuRadioGroup>
+                </DropdownMenuContent>
+            </DropdownMenu>
         </div>
     )
 }
