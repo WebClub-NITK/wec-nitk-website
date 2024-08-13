43c8b22b3c9bf609dd749355fc68191f74bba9ef
marcdhi <gandhi.mardav@gmail.com>
2024-08-13 18:29:47 +0530

    sig: fix offset

--- src/components/our-sigs.jsx
+++ src/components/our-sigs.jsx
@@ -4,13 +4,15 @@ import { ourSigsData } from "../lib/ourSigsData";
 import Image from "next/image";
 import { TracingBeam } from "./ui/tracing-beam";
 import Markdown from "react-markdown";
+import { Button } from "./ui/button";
+
 
 export function OurSIGS() {
   
   return (
     <>
         <h2 className=" text-secondary-800 text-2xl font-semibold lg:text-5xl text-center py-9 mb-10 mt-0">
-          Our SIG's
+          Our Special Interest Groups
         </h2>
 
         <TracingBeam className="px-6">
@@ -20,20 +22,24 @@ export function OurSIGS() {
                 <h1 className={"text-xl mb-4"}>
                   {item.title}
                 </h1>
-    
-                <div className="border-2 rounded-md p-3 text-sm prose prose-sm dark:prose-invert bg-primary-50 md:backdrop-blur-md bg-opacity-70 text-hackclub-secondary-content flex md:flex-row flex-col gap-5 md:items-center md:justify-center md:max-w-[1200px] ">
+                <div className="border-2 rounded-lg p-3 text-sm prose prose-sm dark:prose-invert bg-primary-50 md:backdrop-blur-md bg-opacity-70 text-hackclub-secondary-content flex md:flex-row flex-col gap-5 md:items-center md:justify-center md:max-w-[1200px] ">
                   {item?.imageName && (
                     <Image
                       src={`/${item.imageName}`}
-                      alt="blog thumbnail"
+                      alt="sig thumbnail"
                       height="400"
                       width="400"
-                      className="rounded-lg object-cover"
+                      className="rounded-lg object-cover m-0"
                     />
                   )}
-                  <Markdown className="markdown md:w-[1000px]">
-                    {item.description}
-                  </Markdown>
+                  <div className="md:w-[1000px]">
+                    <Markdown className="markdown ">
+                      {item.description}
+                    </Markdown>
+                    <Button className="p-0">
+                      <a className=" text-primary-950" href={item.sigPage}>Learn More</a>
+                    </Button>
+                  </div>
                 </div>
               </div>
             ))}
--- src/lib/ourSigsData.js
+++ src/lib/ourSigsData.js
@@ -7,7 +7,7 @@ export const ourSigsData = [
       </p>
     ),
     description:
-      "The **Algorithms Special Interest Group (SIG)** of our club is a dynamic community dedicated to exploring the *fascinating world of algorithms*. We unite enthusiasts, students, and professionals who share a passion for understanding and applying algorithmic principles to solve *complex problems*. Our SIG offers a variety of activities, including **lockout tournaments** that challenge your skills in real-time, **mentorship groups** that provide guidance and support, and **post-contest discussions** where we dissect problems and share valuable insights. Whether you're a beginner or looking to refine your expertise, our SIG fosters a *collaborative environment* where members can **learn**, **compete**, and **grow together**.",
+      "The **Algorithms Special Interest Group (SIG)** is a vibrant community where enthusiasts, students, and professionals explore algorithms. We offer activities like **lockout tournaments**, **mentorship groups**, and **post-contest discussions** that help members **learn**, **compete**, and **grow** together. Whether you're a beginner or an expert, our SIG is the perfect place to deepen your algorithmic knowledge.",
     imageName: "algo.png",
     sigPage: "/blogs/8",
     className: " bg-[#ffb098]"
@@ -21,7 +21,7 @@ export const ourSigsData = [
       </p>
     ),
     description:
-      "We focus on **understanding human intelligence** and applying it to machines for the **benefit of humanity**. Our exploration of *Machine Learning (ML)* and *Artificial Intelligence (AI)* emphasizes both cutting-edge research and practical applications. Our research spans **ML theory**, **Deep Learning**, **Reinforcement Learning**, **Data Science**, and their *real-world applications*. We're deeply committed to pushing the boundaries of what these technologies can achieve, both in theory and practice.In addition to our research endeavors, we actively engage in **competitive ML**, participate in **Kaggle contests**, and apply advanced ML techniques to *software products*. Our goal is to bridge the gap between theory and practice, ensuring that our work has a tangible impact on the world. Join us as we explore the *frontiers of AI and ML*, turning innovative ideas into reality for the betterment of society.",
+      "We focus on **understanding human intelligence** and applying it to machines for the **benefit of humanity**. Our work in *Machine Learning (ML)* and *Artificial Intelligence (AI)* combines research with practical applications, covering **ML theory**, **Deep Learning**, **Reinforcement Learning**, and **Data Science**. We also engage in **competitive ML**, **Kaggle contests**, and apply ML to *software products*. Our mission is to bridge theory and practice, creating impactful innovations. Join us as we push the *frontiers of AI and ML* for a better world.",
     imageName: "intel.png",
     sigPage: "/blogs/6",
       className: " bg-[#ffb098]"
@@ -34,7 +34,7 @@ export const ourSigsData = [
       </p>
     ),
     description:
-      "We help students **bridge the gap** between *theory and practice* in software development. In a **peer-to-peer learning** environment, students expand their knowledge, build solutions to real-world problems, and assist communities through **projects and events**. Our work spans **backend**, **frontend**, and **mobile development**, and we are now venturing into **game development**. We've actively participated in **hackathons** and contributed to **open-source projects**. Join us to learn, create, and make an impact!",
+      "We help students bridge the gap between *theory and practice* in software development. In a **peer-to-peer learning** environment, students expand their knowledge, build solutions to real-world problems, and assist communities through **projects and events**. Our work spans **backend**, **frontend**, **mobile development**, and **game development**. We've actively participated in **hackathons** and contributed to **open-source projects**. Join us to learn, create, and make an impact!",
     imageName: "gdsc.png",
     sigPage: "/blogs/7",
       className: " bg-[#ffb098]"
@@ -47,7 +47,7 @@ export const ourSigsData = [
       </p>
     ),
     description:
-      "We are a group of **passionate students** exploring the diverse aspects of *computer systems*. The **Systems and Security SIG** delves into topics like **Networks**, **Distributed Systems**, **Blockchains**, **Security**, **Operating Systems (OS)**, **DBMS**, and **Architecture**. We thrive on working with large **open-source projects** like the Linux kernel. Whether you're into **CTF security challenges** or fascinated by large-scale systems like Netflix, join us at NITK to explore, learn, and innovate together.",
+      "We are a group of passionate students exploring the diverse aspects of *computer systems*. The **Systems and Security SIG** delves into topics like **Networks**, **Distributed Systems**, **Blockchains**, **Security**, **Operating Systems (OS)**, **DBMS**, and **Architecture**. We thrive on working with large **open-source projects** like the Linux kernel. Whether you're into **CTF** security challenges or fascinated by large-scale systems like Netflix, join us to explore, learn, and innovate together.",
     imageName: "systems.png",
     sigPage: "/blogs/9",
     className: " bg-[#ffb098]"
