702414e34d740f6183321c61ddb28f040f314922
suyash101101 <naharsuyash@gmail.com>
2025-02-17 02:29:12 +0530

    Fixed: blogs slug Url

--- frontend/src/helpers/getBlogs.js
+++ frontend/src/helpers/getBlogs.js
@@ -1,5 +1,4 @@
 export async function getBlogs() {
-
     const backendUrl = process.env.NEXT_PUBLIC_STRAPI_URL || 'http://localhost:1337'
 
     const params = new URLSearchParams({
@@ -16,11 +15,32 @@ export async function getBlogs() {
         next: {
           revalidate: parseInt(process.env.REVALIDATE) || 0,
         }
-      })
+    })
 
-      blogs = await blogs.json()
-      blogs = blogs.data || []
+    blogs = await blogs.json()
+    blogs = blogs.data || []
 
+    return blogs
+}
 
-      return blogs
+export async function getBlog(id) {
+    const backendUrl = process.env.NEXT_PUBLIC_STRAPI_URL || 'http://localhost:1337'
+
+    const params = new URLSearchParams({
+        populate: "*",
+    })
+
+    let blog = await fetch(`${backendUrl}/api/blogs/${id}?${params}` , {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer " + process.env.STRAPI_API_KEY,
+        },
+        next: {
+          revalidate: parseInt(process.env.REVALIDATE) || 0,
+        }
+    })
+
+    blog = await blog.json()
+    return blog.data
 }
