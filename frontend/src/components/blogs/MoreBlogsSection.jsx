8d7dee80cb10516938dc6bb7babcc504c7f522d1
Vatsal <vjg239@gmail.com>
2025-02-17 02:16:34 +0530

    [Feat] : Change components for slug implementation

--- frontend/src/components/blogs/MoreBlogsSection.jsx
+++ frontend/src/components/blogs/MoreBlogsSection.jsx
@@ -1,19 +1,31 @@
-import { getLimitedBlogs } from "@/helpers/getLimitedBlogs"
+import { getBlogs } from "@/helpers/getBlogs"
 import MinimalCard from "./MinimalCard"
 
-export default async function({currBlogID}) {
+export default async function MoreBlogsSection({ currBlogID }) {
+    let blogs = await getBlogs()
+    
+    // Filter out the current blog and limit to 3 blogs
+    blogs = blogs
+        ?.filter(blog => blog.id !== currBlogID)
+        ?.slice(0, 3) || []
 
-    let blogs = await getLimitedBlogs(currBlogID)
+    if (!blogs || blogs.length === 0) {
+        return null
+    }
 
-    return(
-        <>
+    return (
+        <div>
             <h2 className="text-lg font-semibold mb-4">More Blogs</h2>
-            <div className=" flex flex-col gap-4 flex-grow">
+            <div className="flex flex-col gap-4 flex-grow">
                 {blogs.map((blog) => (
-                    <MinimalCard key={blog.id} attributes={blog.attributes} blogID={blog.id}/>
+                    <MinimalCard 
+                        key={blog.id} 
+                        attributes={blog.attributes} 
+                        blogID={blog.id}
+                    />
                 ))}
             </div>
-        </>
+        </div>
     )
 }
 
