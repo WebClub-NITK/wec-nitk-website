a07a72c18c0c535bcee7f1ef554191ad182a12ff
Vayun Jain <120264878+vj-004@users.noreply.github.com>
2025-10-06 20:24:16 +0530

    Resolving Event slug issues (issue#48) (#55)
    * Changed hash from #id to #slug
    
    * added event slug redirect page
    
    * added generateMetaData function in event slug page
    
    * Changed hash from #id to #slug
    
    * added event slug redirect page
    
    * added generateMetaData function in event slug page
    
    * events: improve consistency with blogs
    
    ---------
    
    Co-authored-by: Rohith <rohithraichu@gmail.com>
    Co-authored-by: Hariharan <jhariharan2004@gmail.com>

---
+++ frontend/src/app/events/[slug]/page.js
@@ -0,0 +1,39 @@
+import { redirect } from "next/navigation";
+import { getEvent } from "@/helpers/getEvent";
+import { getStrapiMedia } from "@/helpers/strapi_api";
+import { getEventIdFromSlug } from "../event_utils"
+
+export async function generateMetadata({ params }) {
+
+    let event_id = getEventIdFromSlug(params.slug)
+    let event = await getEvent(event_id);
+    const { title, body, date_time, sigs } = event.attributes
+    const cover_image = getStrapiMedia(event.attributes.cover_images.data[0]?.attributes.url)
+
+    return {
+        metadataBase: new URL('https://webclub.nitk.ac.in'),
+        title: title,
+        description: body.substring(0, 200),
+        openGraph: {
+          title: title,
+          description: body.substring(0, 200),
+          url: `https://webclub.nitk.ac.in/events/${params.slug}`,
+          siteName: "WebClub NITK Event",
+          images: [
+            {
+              url: cover_image || 'https://webclub.nitk.ac.in/default-og-image.png',
+              width: 1200,
+              height: 630,
+              alt: title,
+            },
+          ],
+          locale: 'en_US',
+          type: 'article',
+          publishedTime: date_time,
+        },
+      }
+}
+
+export default async function Page({ params }) {
+    redirect(`/events#${params.slug}`);
+}
--- frontend/src/components/events/ExpandableEventCard.jsx
+++ frontend/src/components/events/ExpandableEventCard.jsx
@@ -1,24 +1,32 @@
 "use client"
+
 import { useEffect, useState } from "react"
 import EventCard from "./EventCard"
 import EventModal from "./EventModal"
 import { AnimatePresence, motion } from "framer-motion"
+import { createEventSlug, getEventIdFromSlug } from "@/app/events/event_utils"
 
 export default function ExpandableEventCard ({ event }) {
-    let [ modalInitial, setModalInitial ] = useState(false)
+    let [ modalInitial, setModalInitial ] = useState(null)
+
+    let slug = createEventSlug(event.title, event.id)
+    let isOpen = () => parseInt(getEventIdFromSlug(getHash())) == event.id
     
     useEffect(() => {
-        if (getHashID() == event.id) 
+        if (isOpen()) 
             setModalInitial({ x: 0, y: 0, width: 0, height: 0})
     }, [])
 
     useEffect(() => {
+        if (modalInitial == null) return
+
         if (modalInitial) {
             document.body.style.overflow = "hidden"
-            window.location.hash = event.id
+            window.location.hash = slug
         } else {
             document.body.style.overflow = "auto"
-            if (getHashID() == event.id) {
+            window.location.hash = ""
+            if (isOpen()) {
                 let y = window.scrollY
                 window.location.hash = ""
                 window.scrollTo(0, y)
@@ -51,6 +59,6 @@ export default function ExpandableEventCard ({ event }) {
 }
 
 
-function getHashID () {
-    return parseInt(window.location.hash.slice(1))
+function getHash () {
+    return window.location.hash.slice(1)
 }
