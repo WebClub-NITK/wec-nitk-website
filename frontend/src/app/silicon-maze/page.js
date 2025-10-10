951507bc7522b366b796d759eeb8b746af15a8a2
Hariharan <jhariharan2004@gmail.com>
2025-10-11 02:20:44 +0530

    Added systems link

--- frontend/src/app/silicon-maze/page.js
+++ frontend/src/app/silicon-maze/page.js
@@ -9,7 +9,7 @@ export default function SiliconMazePage () {
                         Algo Tasks
                     </div>
                 </Link>
-                <Link href="https://webclub.nitk.ac.in/" target="_blank">
+                <Link href="https://siliconmaze.wec-ctf.com/" target="_blank">
                     <div className="w-full py-3 px-10 cursor-pointer border-2 border-[#e9e736] rounded-lg bg-primary-900 hover:bg-primary-950 duration-200 hover:shadow-[6px_6px_#dc4f3c] text-white text-3xl font-bold text-center">
                         Systems Tasks
                     </div>
